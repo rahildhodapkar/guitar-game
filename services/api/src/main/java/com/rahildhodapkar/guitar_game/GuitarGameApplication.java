eae170f7977013f36e882459d4e869931deda4e9
Rahil Dhodapkar <rdhodapkar@aahcs.org>
2026-06-30 19:04:49 -0400

    Spring Init

---
+++ services/api/src/main/java/com/rahildhodapkar/guitar_game/GuitarGameApplication.java
@@ -0,0 +1,13 @@
+package com.rahildhodapkar.guitar_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarGameApplication.class, args);
+	}
+
+}
---
+++ services/api/src/test/java/com/rahildhodapkar/guitar_game/GuitarGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahildhodapkar.guitar_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
